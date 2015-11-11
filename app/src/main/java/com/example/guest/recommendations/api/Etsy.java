1bcefffff4a07b0387df943dad01803adab377f8
nifferdil <nifferdil@gmail.com>
2015-11-10 16:59:03 -0800

    add initial files

---
+++ app/src/main/java/com/example/guest/recommendations/api/Etsy.java
@@ -0,0 +1,8 @@
+package com.example.guest.recommendations.api;
+
+/**
+ * Created by Guest on 11/10/15.
+ */
+public class Etsy {
+    private static  final String API_KEY = "yr5fp48j8o215kmrv76ns9fy";
+}
